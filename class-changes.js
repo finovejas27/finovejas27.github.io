0034d0bbd8c6668cfe17f58cbf9ea86ac006e9a9
finovejas27 <franchezcadominic27@gmail.com>
2025-06-11 05:11:32 +1000

    update 6

--- class-changes.js
+++ class-changes.js
@@ -1,13 +1,46 @@
 const classChanges = [
-  { time: 3, target: ".changeme", add: "show" },
-  { time: 9, target: ".changeme", remove: "show" },
-  { time: 9, target: ".changeme", add: "hidden" },
+  { time: 0.9, target: ".largetext", add: "text-black" },
+  { time: 5, target: ".largetext", remove: "text-black" },
 
-  { time: 9, target: ".changeme2", add: "show" },
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
+  { time: 16, target: ".pop1", add: "active" },
+  { time: 16.2, target: ".pop2", add: "active" },
+  { time: 16.4, target: ".pop3", add: "active" },
+  { time: 16.6, target: ".pop4", add: "active" },
+  { time: 16.8, target: ".pop5", add: "active" },
+  { time: 17, target: ".pop6", add: "active" },
 
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
+  { time: 17, target: ".pop1", remove: "active" },
+  { time: 17.2, target: ".pop3", remove: "active" },
+  { time: 17.4, target: ".pop4", remove: "active" },
+  { time: 17.6, target: ".pop2", remove: "active" },
+  { time: 17.8, target: ".pop5", remove: "active" },
+  { time: 18, target: ".pop6", remove: "active" },
+
+  { time: 51, target: "body", add: "active" },
+  { time: 51.5, target: "body", remove: "active" },
+
+  { time: 122, target: ".smalltext", add: "active" },
+  { time: 126, target: ".smalltext", remove: "active" },
+
+  { time: 122, target: ".mediumtext", add: "active" },
+  { time: 126, target: ".mediumtext", remove: "active" },
+
+  { time: 129, target: ".pop7", add: "active" },
+  { time: 129.2, target: ".pop8", add: "active" },
+  { time: 129.4, target: ".pop9", add: "active" },
+  { time: 129.6, target: ".pop10", add: "active" },
+  { time: 129.8, target: ".pop11", add: "active" },
+  { time: 130, target: ".pop12", add: "active" },
+
+  { time: 130, target: ".pop7", remove: "active" },
+  { time: 130.2, target: ".pop8", remove: "active" },
+  { time: 130.4, target: ".pop9", remove: "active" },
+  { time: 130.6, target: ".pop10", remove: "active" },
+  { time: 130.8, target: ".pop11", remove: "active" },
+  { time: 131, target: ".pop12", remove: "active" },
+
+  { time: 151.5, target: "body", add: "active" },
+  { time: 152.4, target: "body", remove: "active" },
+
+
 ];
