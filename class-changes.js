eb7998260784fe17e25a24909f4d7532fa8be95a
finovejas27 <franchezcadominic27@gmail.com>
2025-06-11 13:58:20 +1000

    update 8

--- class-changes.js
+++ class-changes.js
@@ -19,11 +19,11 @@ const classChanges = [
   { time: 51, target: "body", add: "active" },
   { time: 51.5, target: "body", remove: "active" },
 
-  { time: 122, target: ".smalltext", add: "active" },
-  { time: 126, target: ".smalltext", remove: "active" },
+  { time: 53, target: ".smalltext", add: "active" },
+  { time: 103, target: ".smalltext", remove: "active" },
 
-  { time: 122, target: ".mediumtext", add: "active" },
-  { time: 126, target: ".mediumtext", remove: "active" },
+  { time: 53, target: ".mediumtext", add: "active" },
+  { time: 103, target: ".mediumtext", remove: "active" },
 
   { time: 129, target: ".pop7", add: "active" },
   { time: 129.2, target: ".pop8", add: "active" },
@@ -40,7 +40,74 @@ const classChanges = [
   { time: 131, target: ".pop12", remove: "active" },
 
   { time: 151.5, target: "body", add: "active" },
-  { time: 152.4, target: "body", remove: "active" },
+  { time: 152, target: "body", remove: "active" },
+
+  { time: 152.5, target: "body", add: "active" },
+  { time: 153, target: "body", remove: "active" },
+
+  { time: 198, target: "body", add: "active" },
+  { time: 198.5, target: "body", remove: "active" },
+
+  { time: 227, target: ".pop13", add: "active" },
+  { time: 228, target: ".pop14", add: "active" },
+  { time: 229, target: ".pop15", add: "active" },
+  { time: 230, target: ".pop16", add: "active" },
+
+  { time: 231.5, target: ".pop13", remove: "active" },
+  { time: 231.5, target: ".pop17", add: "active" },
+  { time: 232, target: ".pop14", remove: "active" },
+  { time: 232, target: ".pop18", add: "active" },
+  { time: 232.2, target: ".pop15", remove: "active" },
+  { time: 232.2, target: ".pop19", add: "active" },
+  { time: 232.5, target: ".pop16", remove: "active" },
+  { time: 232.5, target: ".pop20", add: "active" },
+
+  { time: 232.8, target: "body", add: "active" },
+  { time: 233, target: "body", remove: "active" },
+
+  { time: 233, target: ".pop17", remove: "active" },
+  { time: 233, target: ".pop18", remove: "active" },
+  { time: 233, target: ".pop19", remove: "active" },
+  { time: 233, target: ".pop20", remove: "active" },
+
+  { time: 265, target: ".pop21", add: "active" },
+  { time: 265, target: ".pop22", add: "active" },
+  { time: 265, target: ".pop23", add: "active" },
+  { time: 265, target: ".pop24", add: "active" },
+  { time: 265, target: ".pop25", add: "active" },
+  { time: 265, target: ".pop26", add: "active" },
+  { time: 265, target: ".pop27", add: "active" },
+  { time: 265, target: ".pop28", add: "active" },
+
+  { time: 266, target: ".pop21", remove: "active" },
+  { time: 266, target: ".pop22", remove: "active" },
+  { time: 266, target: ".pop23", remove: "active" },
+  { time: 266, target: ".pop24", remove: "active" },
+  { time: 266, target: ".pop25", remove: "active" },
+  { time: 266, target: ".pop26", remove: "active" },
+  { time: 266, target: ".pop27", remove: "active" },
+  { time: 266, target: ".pop28", remove: "active" },
+
+  { time: 313, target: ".smalltext", add: "active" },
+  { time: 353, target: ".smalltext", remove: "active" },
+
+  { time: 353, target: "body", add: "active" },
+  { time: 353.5, target: "body", remove: "active" },
+
+  { time: 386, target: ".mediumtext", add: "active" },
+  { time: 410.5, target: ".mediumtext", remove: "active" },
+
+  { time: 386.5, target: "body", add: "active" },
+  { time: 387, target: "body", remove: "active" },
+
+  { time: 387, target: ".smalltext", add: "active" },
+  { time: 410.5, target: ".smalltext", remove: "active" },
+
+  { time: 410, target: "body", add: "active" },
+  { time: 410.5, target: "body", remove: "active" },
+
+  
+
 
 
 ];
